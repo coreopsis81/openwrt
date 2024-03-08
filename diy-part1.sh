021f05133e9789e65317c7525cd86c8494757f08
coreopsis81 <148996250+coreopsis81@users.noreply.github.com>
2024-03-08 15:30:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,15 +8,44 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/files target/linux/x86/files
+rm -rf target/linux/x86/files/.svn
 
-# Lienol/openwrt-package
-#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/patches-5.15 target/linux/x86/patches-5.15
+rm -rf target/linux/x86/patches-5.15/.svn
+
+curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -o package/kernel/linux/modules/video.mk
+curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/base-files/etc/board.d/02_network -o target/linux/x86/base-files/etc/board.d/02_network
+
+sed -i "s/ +PACKAGE_kmod-backlight:kmod-backlight//" package/kernel/linux/modules/video.mk
+
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci usbutils pciutils lm-sensors-detect kmod-alx kmod-vmxnet3 kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-r8125 kmod-8139cp kmod-8139too kmod-i40e kmod-drm-i915 kmod-drm-amdgpu kmod-mlx4-core kmod-mlx5-core fdisk lsblk kmod-phy-broadcom/' target/linux/x86/Makefile
+
+
+mv -f tmp/r81* feeds/kiddin9/
+sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
+sed -i 's/256/1024/g' target/linux/x86/image/Makefile
+
+echo '
+CONFIG_ACPI=y
+CONFIG_X86_ACPI_CPUFREQ=y
+CONFIG_NR_CPUS=512
+CONFIG_MMC=y
+CONFIG_MMC_BLOCK=y
+CONFIG_SDIO_UART=y
+CONFIG_MMC_TEST=y
+CONFIG_MMC_DEBUG=y
+CONFIG_MMC_SDHCI=y
+CONFIG_MMC_SDHCI_ACPI=y
+CONFIG_MMC_SDHCI_PCI=y
+CONFIG_DRM_I915=y
+' >> ./target/linux/x86/config-5.15
+
+# Modify default IP 更改IP地址
+sed -i 's/192.168.1.1/192.168.0.251/g' package/base-files/files/bin/config_generate
+
+# 把网口0替换为网口1
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
