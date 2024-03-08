de8f35420cf1fb8c4b68d009df940ecdaa756524
coreopsis81 <148996250+coreopsis81@users.noreply.github.com>
2024-03-08 17:16:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,10 @@
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 bash $SHELL_FOLDER/../common/kernel_5.15.sh
 
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/files target/linux/x86/files
+svn co https://github.com/coolsnowwolf/lede/tree/master/target/linux/x86/files target/linux/x86/files
 rm -rf target/linux/x86/files/.svn
 
-svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/x86/patches-5.15 target/linux/x86/patches-5.15
+svn co https://github.com/coolsnowwolf/lede/tree/master/target/linux/x86/patches-5.15 target/linux/x86/patches-5.15
 rm -rf target/linux/x86/patches-5.15/.svn
 
 curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -o package/kernel/linux/modules/video.mk
