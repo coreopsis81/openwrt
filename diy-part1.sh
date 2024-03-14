f7534230058e317c611257e5d8bb0a3e9632f7b5
coreopsis81 <148996250+coreopsis81@users.noreply.github.com>
2024-03-14 15:46:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,28 +8,6 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-bash $SHELL_FOLDER/../common/kernel_5.15.sh
-
-# svn co https://github.com/coolsnowwolf/lede/tree/master/target/linux/x86/files target/linux/x86/files
-# rm -rf target/linux/x86/files/.svn
-
-# svn co https://github.com/coolsnowwolf/lede/tree/master/target/linux/x86/patches-5.15 target/linux/x86/patches-5.15
-# rm -rf target/linux/x86/patches-5.15/.svn
-
-curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -o package/kernel/linux/modules/video.mk
-curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/x86/base-files/etc/board.d/02_network -o target/linux/x86/base-files/etc/board.d/02_network
-
-sed -i "s/ +PACKAGE_kmod-backlight:kmod-backlight//" package/kernel/linux/modules/video.mk
-
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-usb-hid kmod-mmc kmod-sdhci usbutils pciutils lm-sensors-detect kmod-alx kmod-vmxnet3 kmod-igbvf kmod-iavf kmod-bnx2x kmod-pcnet32 kmod-tulip kmod-r8125 kmod-8139cp kmod-8139too kmod-i40e kmod-drm-i915 kmod-drm-amdgpu kmod-mlx4-core kmod-mlx5-core fdisk lsblk kmod-phy-broadcom/' target/linux/x86/Makefile
-
-# mv -f tmp/r81* feeds/kiddin9/
-# mv -f https://github.com/kiddin9/openwrt-packages/tree/master/r81* feeds
-
-sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
-# sed -i 's/256/1024/g' target/linux/x86/image/Makefile
-
 echo '
 CONFIG_ACPI=y
 CONFIG_X86_ACPI_CPUFREQ=y
